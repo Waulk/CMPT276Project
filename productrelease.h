d042a6f6187310ea031d1e9c1c05538fd553aa24
makih2003 <139406942+makih2003@users.noreply.github.com>
2024-06-28 12:15:03 -0700

    addHfiles

---
+++ productrelease.h
@@ -0,0 +1,20 @@
+/***********************************************
+ * Module: productrelease.h
+ * 
+ * Code Version History: 
+ * Ver. 1: - 2024-06-28 Original by Maki Hosokawa
+ *         - Initial version 
+ ***********************************************/
+
+/***********************************************
+ * Example: The ProductRelease class provides blah blah blah......
+ * 
+ * Below is the instructions/tips from Assignment 3 instruction paper:
+ * It is important to state the nature of the cohesion of the module or the nature of (not implementation of) the data or control abstraction retained by/hidden in the module. You may also mention any general rules as to how the module should be used. e.g. "You must call CreateStack() before you can use a stack"
+***********************************************/
+
+/***********************************************
+ * #ifndef PRODUCTRELEASE_H
+ * #define PRODUCTRELEASE_H
+ * #include <iostream>
+ ***********************************************/
